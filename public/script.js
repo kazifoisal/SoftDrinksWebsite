98c105f838f8c946096dc831e949e61c40ea8ccf
Kazi Foisal <kazimdfoisal7409@gmail.com>
2024-05-12 03:43:06 +0600

    Created some carts with animation

--- public/script.js
+++ public/script.js
@@ -1,35 +1,102 @@
-
-
 var tl = gsap.timeline({
-    scrollTrigger:{
-      trigger: "#two",
-      start: "0% 95%",
-      end: "70% 40%",
-      scrub: true,
-    }
-  });
+  scrollTrigger: {
+    trigger: "#two",
+    start: "0% 95%",
+    end: "70% 40%",
+    scrub: true,
+  },
+});
 
-tl.to("#fanta",{
-         width:"40%",
-        top: "130%",
-        left: "0%"
-    }, 'orange')
-    
-    tl.to("#orange-cut",{
-    top:"148%",
-    left: "5%"
-}, 'orange')
+tl.to(
+  "#fanta",
+  {
+    width: "40%",
+    top: "130%",
+    left: "0%",
+  },
+  "orange"
+);
 
-tl.to("#orange",{
+tl.to(
+  "#orange-cut",
+  {
+    top: "148%",
+    left: "5%",
+  },
+  "orange"
+);
+
+tl.to(
+  "#orange",
+  {
     width: "17%",
-    top:"170%",
-    left: "80%"
-}, 'orange')
+    top: "169%",
+    left: "80%",
+  },
+  "orange"
+);
 
-tl.to("#leaf",{
-    top:"120%",
+tl.to(
+  "#leaf",
+  {
+    top: "120%",
     rotate: "130deg",
-    left: "90%",
-    zIndex:1
-}, 'orange')
+    left: "85%",
+    zIndex: 1,
+  },
+  "orange"
+);
 
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#three",
+    start: "0% 95%",
+    end: "70% 40%",
+    scrub: true,
+  },
+});
+
+
+tl2.from(
+  "#pepsi",
+  { opacity: 0,
+    x: "-100", 
+    ease: "power1.out", 
+    duration: 1 },
+  "ca"
+);
+tl2.from("#iceCube", { 
+  opacity: 0, 
+  y: -30, 
+  duration: 1 
+}, "ca");
+
+tl2.from(
+  "#sprite",
+  { opacity: 0,
+     x: 180, 
+     ease: "power1.out",
+      duration: 1 },
+  "ca"
+);
+tl2.from("#lemon", { opacity: 0, y: -30, duration: 1 }, "ca");
+
+tl2.to(
+  "#orange-cut",
+  {
+    width: "18%",
+    left: "41%",
+    top: "200%",
+  },
+  "ca"
+);
+
+tl2.to(
+  "#fanta",
+  {
+    width: "33%",
+    top: "213%",
+    left: "34%",
+  },
+  "ca"
+);
